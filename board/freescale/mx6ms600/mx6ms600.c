74757462367761b99635c15375d1baadc6973749
kevin <huhuikevin@gmail.com>
2016-09-11 22:17:08 +0800

    EMMC5.0 works
    Signed-off-by: kevin <huhuikevin@gmail.com>

--- board/freescale/mx6ms600/mx6ms600.c
+++ board/freescale/mx6ms600/mx6ms600.c
@@ -47,10 +47,15 @@ DECLARE_GLOBAL_DATA_PTR;
 #define UART_PAD_CTRL  (PAD_CTL_PUS_100K_UP |			\
 	PAD_CTL_SPEED_MED | PAD_CTL_DSE_40ohm |			\
 	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
-
+#if 1
 #define USDHC_PAD_CTRL (PAD_CTL_PUS_47K_UP |			\
 	PAD_CTL_SPEED_LOW | PAD_CTL_DSE_80ohm |			\
 	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+#else
+#define USDHC_PAD_CTRL (PAD_CTL_PKE | PAD_CTL_PUE |    \  
+        PAD_CTL_PUS_22K_UP | PAD_CTL_SPEED_LOW |    \  
+        PAD_CTL_DSE_120ohm | PAD_CTL_SRE_FAST | PAD_CTL_HYS)  
+#endif
 
 #define SPI_PAD_CTRL (PAD_CTL_HYS | PAD_CTL_SPEED_MED | \
 		      PAD_CTL_DSE_40ohm | PAD_CTL_SRE_FAST)
@@ -511,6 +516,7 @@ int mmc_get_env_devno(void)
 	if (is_boot_from_usb())
 		return CONFIG_SYS_MMC_ENV_DEV;
 #endif
+	//return CONFIG_SYS_MMC_ENV_DEV;
 
 	bootsel = (soc_sbmr & 0x000000FF) >> 6 ;
 
